dd3f9bdcd214bda0d35bbcf0289a22497be666f0
Phnc <paulohn27@hotmail.com>
2022-05-22 22:12:34 -0300

    update player prefs constants

--- Assets/Shared/Constants/PlayerPrefConstants.cs
+++ Assets/Shared/Constants/PlayerPrefConstants.cs
@@ -9,9 +9,9 @@ public static class PlayerPrefConstants
     public static class SkillsRelated
     {
         public const string PlayerSpeed = "speed";
-        public const float PlayerDefaultSpeedValue = 2000;
-        public const float PlayerIncreaseSpeedValue = 1000;
-        public const float PlayerDecreaseSpeedValue = 500;
+        public const float PlayerDefaultSpeedValue = 300;
+        public const float PlayerIncreaseSpeedValue = 700;
+        public const float PlayerDecreaseSpeedValue = 150;
         public const string EnemySpeed = "enemySpeed";
         public const float EnemyDefaultSpeedValue = 2;
         public const float EnemyDecreaseSpeedValue = 0.5f;
